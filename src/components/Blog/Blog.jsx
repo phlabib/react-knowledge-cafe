c853144ed046462f9b8c70eabe3327b1b6c06617
phlabib <ph.labib@gmail.com>
2023-09-14 10:21:58 +0600

    Remove bookmark after read and deploy to surge

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -4,7 +4,7 @@ import Blogs from '../Blogs/Blogs';
 
 
 const Blog = ({blog, handleAddToBookmark, handleMarkAsRead}) => {
-   const {title, cover, author, author_img, posted_date, reading_time, hashtags } = blog;
+   const {id, title, cover, author, author_img, posted_date, reading_time, hashtags } = blog;
     return (
         <div className='mb-20 space-y-4'>
             <img className='w-full mb-8' src= {cover} alt= {`Cover Picture of the title ${title}`} />
@@ -33,7 +33,7 @@ const Blog = ({blog, handleAddToBookmark, handleMarkAsRead}) => {
             }
             </p>
             <button 
-            onClick={() => handleMarkAsRead(reading_time)}
+            onClick={() => handleMarkAsRead( id, reading_time)}
             className='text-purple-800 font-bold underline'
             >Mark As Read</button>
 
--- src/components/Bookmarks/Bookmarks.jsx
+++ src/components/Bookmarks/Bookmarks.jsx
@@ -5,12 +5,12 @@ import Bookmark from '../Bookmark/Bookmark';
 const Bookmarks = ({bookmarks, readingTime}) => {
     return (
         <div className="md: w-1/3 bg-gray-400 rounded-xl p-4 m-4" >
-            <div>
-                <h3 className="text-3xl">Reading Time : {readingTime}</h3>
+            <div >
+                <h3 className="text-3xl  text-center bg-yellow-200 rounded-xl p-4 m-4 ">Reading Time : {readingTime}</h3>
             </div>
             <h2 className="text-3xl text-center   ">Bookmarked Blogs : {bookmarks.length}</h2>
             {
-               bookmarks.map(bookmark => <Bookmark key={bookmark.id} bookmark={bookmark}></Bookmark>) 
+               bookmarks.map((bookmark, idx) => <Bookmark key={idx} bookmark={bookmark}></Bookmark>) 
             }
         </div>
     );
@@ -21,4 +21,4 @@ Bookmarks.PropTypes = {
     bookmarks : PropTypes.array,
     readingTime : PropTypes.number
 }
-export default Bookmarks;
+export default Bookmarks; 
